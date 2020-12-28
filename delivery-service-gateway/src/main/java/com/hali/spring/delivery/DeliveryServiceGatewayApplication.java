eecf7d4fcee0f71b07c01a1b72bb01e73dbab63e
haiderali22 <58180942+haiderali22@users.noreply.github.com>
2020-12-28 22:07:07 +0500

    added gateway config

---
+++ delivery-service-gateway/src/main/java/com/hali/spring/delivery/DeliveryServiceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hali.spring.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryServiceGatewayApplication.class, args);
+	}
+
+}
---
+++ delivery-service-gateway/src/test/java/com/hali/spring/delivery/DeliveryServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hali.spring.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
